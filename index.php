4c29a6c60dc51261c63e0e3c61f3259bbf859a85
mauriciog150 <mra.garcia@yavirac.edu.ec>
2022-09-05 10:08:29 -0500

    se agrega el proyecto

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include "./inc/sesion_start.php"; ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php include "./inc/head.php"; ?>
+</head>
+<body>
+    <?php
+        include "./inc/navbar.php";
+        include "./inc/script.php";
+    ?>
+</body>
+</html>
