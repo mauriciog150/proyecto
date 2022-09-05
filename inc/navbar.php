ecab6b515a2ffe33699a0662bb6d3adbd9185554
mauriciog150 <mra.garcia@yavirac.edu.ec>
2022-09-05 15:17:35 -0500

    conexion base de datos

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
+    <a class="navbar-item" href="index.php?vista=home">
       <img src="./img/Logo_Aterrizar.jpeg" width="150" height="50">
     </a>
 
--- index.php
+++ index.php
@@ -7,8 +7,25 @@
 </head>
 <body>
     <?php
-        include "./inc/navbar.php";
-        include "./inc/script.php";
+        if(!isset($_GET['vista']) || $_GET['vista']== ""){
+            $_GET['vista']="login";
+        }
+        if(is_file("./vistas/".$_GET['vista'].".php") && $_GET['vista']!="login" && $_GET['vista']!="404"){
+
+            include "./inc/navbar.php";
+            include "./vistas/".$_GET['vista'].".php";
+            include "./inc/script.php";
+
+        }else{
+            if($_GET['vista']=="login"){
+                include "./vistas/login.php";
+            }else{
+                include "./vistas/404.php";
+            }
+
+        }
+
+        
     ?>
 </body>
 </html>
